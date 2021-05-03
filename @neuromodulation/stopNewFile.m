aba92fe95e0d1d14062441cca20a6fa47146ba2d
Your Name <you@example.com>
2021-05-03 20:45:04 +0200

    functions for quick analyze added scp saved as struct

--- @neuromodulation/stopNewFile.m
+++ @neuromodulation/stopNewFile.m
@@ -24,16 +24,30 @@ if self.use_tiepie
         pause(1e-3)
     end
     self.data_tiepie = self.scp.getData();
+    
 end
+
+scp = struct(self.scp); %#ok<NASGU>
+
+% save
+save( self.fpath, 'self', 'dataBVA', 'scp' )
+fprintf('done ! \n')
 
 % get comments from the GUI
 self.comments = self.GUIdata.edit_comments.String;
 
-% save
-save( self.fpath, 'self', 'dataBVA' )
-fprintf('done ! \n')
+
+%% Quick analyze & plot
+
+[tremor_assessment, left_data, right_data] = analyze_neuromod_data(self, dataBVA, 'imposed_freq_tremor', [] );
+
+visualize_tremor_assessment_recording(left_data, right_data, tremor_assessment)
+
+
+%% Reset
 
 % reset, ready for a new session
 self.resetData();
+
 
 end % function
--- start_neuromodulationGUI.m
+++ start_neuromodulationGUI.m
@@ -1,6 +1,10 @@
 clc
 clear
 
+addpath( fullfile( fileparts(pwd), 'vis_stim_prisma_neuromod'                               ) )
+addpath( fullfile( fileparts(pwd), 'vis_stim_prisma_neuromod', 'functions'                  ) )
+addpath( fullfile( fileparts(pwd), 'vis_stim_prisma_neuromod', 'external_lib', 'hline_vline') )
+
 nm = neuromodulation();
 disp(nm)
 
